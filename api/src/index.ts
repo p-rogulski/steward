84d28f0b8f8affa5b50f8009246d29a88001380f
Piotr Rogulski <progulski@sii.pl>
2020-03-12 19:53:05 +0100

    Nodemon support for api

--- api/src/index.ts
+++ api/src/index.ts
@@ -4,7 +4,7 @@ import express = require('express');
 const app: express.Application = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello Worldsxts!');
+  res.send('Test send css!');
 });
 
 const server=app.listen(4921, function () {
