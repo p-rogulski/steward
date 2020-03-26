ffd19946b271b9a27ba503a6d8f80b2a43208744
Piotr Rogulski <progulski@sii.pl>
2020-03-26 12:43:20 +0100

    CSS changes for homepage

--- web/src/app/modules/home/pages/homepage/homepage.component.ts
+++ web/src/app/modules/home/pages/homepage/homepage.component.ts
@@ -8,7 +8,7 @@ import { faPlug } from '@fortawesome/free-solid-svg-icons';
 export class HomepageComponent implements OnInit {
 
   @Input() logoUrl: string = '';
-  plug = '../../../assets/img/plug.png';
+  termsAndConditons = '../../../assets/img/terms-and-conditions.png';
   books = '../../../assets/img/books.png';
   cake = '../../../assets/img/cake.png';
   compare = '../../../assets/img/compare.png';
