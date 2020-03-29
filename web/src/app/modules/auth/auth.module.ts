43996e3afd0e1c1a55532e8b3e9482652f266b08
rogulskip <piotr.rgl@gmail.com>
2020-03-29 18:14:43 +0200

    Conceptual mock for registration without backend interaction

--- web/src/app/modules/auth/auth.module.ts
+++ web/src/app/modules/auth/auth.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {SignInComponent} from './pages/sign-in/sign-in.component';
+import {SignUpComponent} from './pages/sign-up/sign-up.component';
 import { AuthRoutingModule } from './auth-routing.module';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [SignInComponent, SignUpComponent ],
   imports: [
     CommonModule,
     AuthRoutingModule
